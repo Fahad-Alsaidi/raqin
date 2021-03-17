f2c9f6a40727aca9eb6f728564ef0f5ab3afc618
Sulaiman Said <suliman.mazro3i@gmail.com>
2021-03-17 23:51:12 +0400

    new: :tada: added raqin ui example to build on

---
+++ raqin-ui/src/pages/preferences.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Preferences Page</h1>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: "PageIndex",
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
