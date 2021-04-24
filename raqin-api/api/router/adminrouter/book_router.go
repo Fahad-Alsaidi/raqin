c1cc3a84a817ed101fdcd404305b844fe9321355
Sulaiman Said <suliman.mazro3i@gmail.com>
2021-04-24 04:48:18 +0400

    chore: :arrow_up: change route controller name

--- raqin-api/api/router/adminrouter/book_router.go
+++ raqin-api/api/router/adminrouter/book_router.go
@@ -12,5 +12,5 @@ func bookRouter(gRoute *echo.Group) {
 	db := storage.DBInstance()
 	bookCtrl := book.NewBookController(book.NewBookService(book.NewBookRepo(db)))
 
-	gRoute.POST("/upload", bookCtrl.UploadNewBook)
+	gRoute.POST("/upload", bookCtrl.NewBook)
 }
