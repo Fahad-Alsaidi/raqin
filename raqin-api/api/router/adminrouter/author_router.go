252c9c8e3fab2aec9d306a24e780533b9756c10b
Sulaiman Said <suliman.mazro3i@gmail.com>
2021-04-25 05:20:05 +0400

    new: :tada: add authorRouter

---
+++ raqin-api/api/router/adminrouter/author_router.go
@@ -0,0 +1,16 @@
+package adminrouter
+
+import (
+	"raqin-api/app/author"
+	"raqin-api/storage"
+
+	"github.com/labstack/echo/v4"
+)
+
+func authorRouter(gRoute *echo.Group) {
+	// init NewBookController with NewBookService
+	db := storage.DBInstance()
+	authorCtrl := author.NewAuthorController(author.NewBookService(author.NewAuthorRepo(db)))
+
+	gRoute.POST("/new", authorCtrl.NewAuthor)
+}
