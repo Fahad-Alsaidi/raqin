c66de99e8cc4b159265fbd6fcfee124a8b6b1abe
Sulaiman Said <suliman.mazro3i@gmail.com>
2021-12-03 16:35:00 +0400

    fix: add some validations to backend

--- raqin-backend/api/page/controllers/page.js
+++ raqin-backend/api/page/controllers/page.js
@@ -15,26 +15,32 @@ module.exports = {
    */
 
   async create(ctx) {
-    const { id } = ctx.params;
 
     let entity;
 
-    const [page] = await strapi.services.page.find({
+    const userId = ctx.state.user.id;
+    ctx.request.body.user = userId;
+    const page = await strapi.services.page.findOne({
       pageNumber: ctx.request.body.pageNumber,
       'book.id': ctx.request.body.book,
-      'user.id': ctx.state.user.id
+      'user.id': userId
     });
 
     if (page) {
-      return ctx.response.conflict('resource already exists');
+      return ctx.response.conflict('page already exists');
+    }
+
+    const book = await strapi.services.book.findOne({
+      id: ctx.request.body.book,
+    });
+    if (ctx.request.body.pageNumber > book?.numberOfPages) {
+      return ctx.response.conflict(`number of page can't exceed max number of book pages`);
     }
 
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
-      data.user = ctx.state.user.id;
       entity = await strapi.services.page.create(data, { files });
     } else {
-      ctx.request.body.user = ctx.state.user.id;
       entity = await strapi.services.page.create(ctx.request.body);
     }
     return sanitizeEntity(entity, { model: strapi.models.page });
@@ -51,14 +57,23 @@ module.exports = {
 
     let entity;
 
-    const [page] = await strapi.services.page.find({
-      id: ctx.params.id,
-      'user.id': ctx.state.user.id,
+    const userId = ctx.state.user.id;
+    const page = await strapi.services.page.findOne({
+      id: id,
+      'user.id': userId,
     });
 
     if (!page) {
       return ctx.unauthorized(`You can't update this entry`);
     }
+    if (page.stage == "rev" || page.stage == "done") {
+      return ctx.locked(`page is already in revision state or has been approved`);
+    }
+
+    // delete attributes that shouldn't be updated
+    delete ctx.request.body.pageNumber;
+    delete ctx.request.body.book;
+    delete ctx.request.body.user;
 
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
@@ -83,13 +98,16 @@ module.exports = {
 
     let entity;
 
-    const [page] = await strapi.services.page.find({
+    const page = await strapi.services.page.findOne({
       id: ctx.params.id,
       'user.id': ctx.state.user.id,
     });
 
     if (!page) {
       return ctx.unauthorized(`You can't delete this entry`);
+    }
+    if (page.stage == "rev" || page.stage == "done") {
+      return ctx.locked(`page can't be deleted if in revision state or has been approved`);
     }
 
     if (ctx.is('multipart')) {
